b4a6e38c2d117e9e6f593f6267b59ef9868314a2
Davide Santangelo <davide.santangelo@gmail.com>
2016-11-28 10:48:28 +0100

    Update encrypt_controller.rb

--- app/controllers/api/encrypt_controller.rb
+++ app/controllers/api/encrypt_controller.rb
@@ -2,8 +2,8 @@ require 'crypto'
 require 'securerandom'
 
 class Api::EncryptController < Api::BaseController
-	def encrypt
-		encrypt = Crypto.encrypt(params[:text], SecureRandom.urlsafe_base64)
-	  render status: 200, json: { data: encrypt }
-	end
+  def encrypt
+    encrypt = Crypto.encrypt(params[:text], SecureRandom.urlsafe_base64)
+    render status: 200, json: { data: encrypt }
+  end
 end
