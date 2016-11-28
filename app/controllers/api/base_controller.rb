c9c8cefa72163598200a4ea58d52eb9a5c1d2827
Davide Santangelo <davide.santangelo@gmail.com>
2016-11-28 10:47:59 +0100

    Update base_controller.rb

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,20 +1,20 @@
 class Api::BaseController < ApplicationController
-	skip_before_filter :verify_authenticity_token
-	before_filter :set_headers
+  skip_before_filter :verify_authenticity_token
+  before_filter :set_headers
 
-	def me
-		render status: :ok, json: {}
-	end
+  def me
+    render status: :ok, json: {}
+  end
 	
-	def options
-		set_headers
-	  # this will send an empty request to the client with 200 status code (OK, can proceed)
-	  render :text => '', :content_type => 'text/plain'
-	end
+  def options
+    set_headers
+    # this will send an empty request to the client with 200 status code (OK, can proceed)
+    render :text => '', :content_type => 'text/plain'
+  end
 
-	protected
-
-	# Set CORS
+  protected
+	
+  # Set CORS
   def set_headers
     headers["Access-Control-Allow-Origin"] = '*'
     headers['Access-Control-Expose-Headers'] = 'Etag'
